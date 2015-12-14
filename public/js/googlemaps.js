dddaa94e00bfc08d19a4151d39e5dcf566590ad0
Ben Ebel <bge1234@gmail.com>
2015-12-14 12:44:46 -0700

    added initial marker

--- public/js/googlemaps.js
+++ public/js/googlemaps.js
@@ -1,12 +1,18 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: {lat: 39.757837, lng: -105.0073}
+    center: {lat: 39.757785, lng: -105.007142}
   });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 39.757785, lng: -105.007142},
+    map: map
+  });
+
   var geocoder = new google.maps.Geocoder();
 
   document.getElementById('send').addEventListener('click', function() {
-  geocodeAddress(geocoder, map);
+    geocodeAddress(geocoder, map);
   });
 }
 
