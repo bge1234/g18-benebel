c0d691c7adb2ad9f3e5273eb485b8749ce0b1017
Ben Ebel <bge1234@gmail.com>
2015-12-17 14:33:04 -0700

    added warning and link to CORS Chrome extension

--- public/js/strava.js
+++ public/js/strava.js
@@ -1,3 +1,5 @@
+console.log("Make sure to install and enable the CORS Chrome extension: https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi");
+
 var count = 0;
 var endTimeLast = Date.now();
 
